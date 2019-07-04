77593cab56fb9981048557135092e2a6b9855f6d
ZQW <phyer219@gmail.com>
2019-07-04 20:36:04 +0800

    correct NSRcal

--- source/_posts/2019-07-03-专业笔记-NSRcal/FreeEnergy.py
+++ source/_posts/2019-07-03-专业笔记-NSRcal/FreeEnergy.py
@@ -3,12 +3,12 @@ import numpy as np
 from scipy import integrate
 zero = 1e-1
 cutoff = 1
-err = 1.49e-1
-mu = 0
+err = 1.49
+mu = 1
 beta = 1
 R = 1/30
 omega = 1
-eps = .1                        # infinite small imaginary part 
+eps = 1                       # infinite small imaginary part 
 rkv = 0                         # 2R_p/(k_n^2 v_p)
 def xi(k, mu=mu):
     xi = k**2 - mu
@@ -37,7 +37,30 @@ def PI(omega, q, eps=eps, mu=mu, zero=zero, cutoff=cutoff,
     I = Irel -1j * Iimg
     PI = 2/(np.pi**2) * I
     return PI
-def PIr(q, omega, R=R, mu=mu, zero=zero, cutoff=cutoff, err=err):
+
+
+
+'''
+print(np.imag(PI(10,1)))
+N = 1000
+q = np.linspace(0.1,1,N)
+II = np.zeros(N)
+for i in range(N):
+    II[i] = np.real(PI(10,q[i]))
+plt.plot(q,II,'gray')
+
+II = np.zeros(N)
+for i in range(N):
+    II[i] = np.real(PI(5,q[i]))
+plt.plot(q,II,'r')
+
+
+plt.show()
+'''
+
+
+
+def PIr(omega, q, R=R, mu=mu, zero=zero, cutoff=cutoff, err=err):
     I, error = integrate.quad(lambda k: k**2, zero, cutoff, epsabs=err)
     PIr = -1/(np.pi**2) * I
     I, error = integrate.quad(lambda k: 1, zero, cutoff, epsabs=err)
@@ -46,19 +69,55 @@ def PIr(q, omega, R=R, mu=mu, zero=zero, cutoff=cutoff, err=err):
     PIr = R * PIr
     return PIr
 
+
+
+'''
+N = 1000
+q = np.linspace(0.1,1,N)
+II = np.zeros(N)
+for i in range(N):
+    II[i] = np.imag(PIr(10,q[i]))
+plt.plot(q,II,'gray')
+for i in range(N):
+    II[i] = np.imag(PIr(1,q[i]))
+plt.plot(q,II,'r')
+
+plt.show()
+'''
+
+
+
 def delta(omega, q, rkv):
-    delta = rkv/(4*np.pi) + z(omega, q) + PIr(q, omega)
+    delta = rkv/(4*np.pi) + z(omega, q) + PIr(omega, q)
     delta = np.angle(delta)
     return delta
 
-def omegaInt(rkv, mcutoff=-cutoff, pcutoff=cutoff, cutoff=cutoff):
-    f = lambda q, omega: 3 * q**2 * n(omega) * delta (q, omega, rkv)
-    omegaInt, error = integrate.dblquad(f, mcutoff, pcutoff, lambda
-                                        omega: 0, lambda omega: cutoff
-                                        , epsabs=err) 
+
+
+'''
+N = 1000
+q = np.linspace(0.1,1,N)
+II = np.zeros(N)
+for i in range(N):
+    II[i] = delta(10,q[i],0)
+plt.plot(q,II,'gray')
+for i in range(N):
+    II[i] = delta(5,q[i],0)
+plt.plot(q,II,'r')
+
+plt.show()
+'''
+
+ 
+def omegaInt(rkv, mcutoff=-cutoff, pcutoff=cutoff, cutoff=cutoff,
+    beta=beta):
+    f = lambda q, omega: 3 * q**2 * (1/(np.exp(beta*omega)-1))\
+                                  * delta(omega, q, rkv) 
+    omegaInt, error = integrate.dblquad(f, -.1, pcutoff, lambda
+                                        omega: zero, lambda omega:
+                                        cutoff, epsabs=err)
     return omegaInt
 
-
-
 print(omegaInt(0))
+
 
