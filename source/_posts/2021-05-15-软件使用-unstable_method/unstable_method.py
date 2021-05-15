797ea0c9dd055fccceb4449f06967bef3aa13d29
zqw <phyer219@gmail.com>
2021-05-15 15:59:15 +0800

    new post

---
+++ source/_posts/2021-05-15-软件使用-unstable_method/unstable_method.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def unstable(n: int) -> float:
+    p = []
+    p.append(1)
+    p.append((np.sqrt(5) - 1) / 2)
+    for i in range(n-1):
+        i += 1
+        p.append(p[i-1] - p[i])
+    return p[-1]
+
+
+def stable(n: int) -> float:
+    return ((np.sqrt(5) - 1) / 2)**n
+
+
+n = 80
+x = []
+y_unstable = []
+y_stable = []
+for i in range(n):
+    i += 1
+    x.append(i)
+    y_unstable.append(unstable(i))
+    y_stable.append(stable(i))
+
+plt.plot(x, y_unstable, '-x', label="unstable")
+plt.plot(x, y_stable, label="stable")
+plt.xlabel(r'$n$')
+plt.ylabel(r'$\left(\frac{\sqrt{5} - 1}{2}\right)^n$')
+plt.legend()
+plt.savefig('unstable_method.png')
