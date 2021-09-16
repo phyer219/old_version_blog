7c206c02a9afc020f21c80912596079e0ef73611
zqw <phyer219@gmail.com>
2021-09-16 14:15:13 +0800

    new post

---
+++ source/_posts/2021-09-16-scipy_integrate/code.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy import integrate
+import matplotlib.pyplot as plt
+
+nodes = []
+
+
+def foo(x):
+    nodes.append(x)
+    return np.exp(x) + np.sin(10*x)**2 + np.exp(x)**100
+
+
+result = integrate.quad(foo, -1, 1, epsrel=1e-125, limit=2)
+n = np.linspace(1, len(nodes), len(nodes))
+print(len(nodes))
+print(len(nodes)/21)
+plt.plot(n, nodes, '*')
+plt.grid()
+plt.title('limit = 2')
+plt.xlabel(r"$i$")
+plt.ylabel("position of $i$th node")
+plt.savefig('fig.png')
