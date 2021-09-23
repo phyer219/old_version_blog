d532530618999b72ba4235d65f47e589316de96e
zqw <phyer219@gmail.com>
2021-09-23 15:27:23 +0800

    update post

--- source/_posts/2021-09-16-scipy_integrate/code.py
+++ source/_posts/2021-09-16-scipy_integrate/code.py
@@ -10,10 +10,11 @@ def foo(x):
     return np.exp(x) + np.sin(10*x)**2 + np.exp(x)**100
 
 
-result = integrate.quad(foo, -1, 1, epsrel=1e-125, limit=2)
+result = integrate.quad(foo, -1, 1, epsrel=1e-125, limit=2, full_output=1)
 n = np.linspace(1, len(nodes), len(nodes))
 print(len(nodes))
 print(len(nodes)/21)
+print(result[2]['neval'])
 plt.plot(n, nodes, '*')
 plt.grid()
 plt.title('limit = 2')
